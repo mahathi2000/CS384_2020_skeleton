d0fd599019e13d65e30a46be2c8be6cf9e7d0f15
Dr. Mayank Agarwal <cs3842020@gmail.com>
2020-09-15 21:30:38 +0530

    Assignment 1

---
+++ Assignment1/tutorial01.py
@@ -0,0 +1,66 @@
+# Function to add two numbers 
+def add(num1, num2): 
+	addition = num1 + num2
+	return addition
+
+# Function to subtract two numbers 
+def subtract(num1, num2): 
+	subtraction = num1 - num2
+	return subtraction
+
+# Function to multiply two numbers 
+def multiply(num1, num2): 
+	multiplication = num1 * num2
+	return multiplication
+
+# Function to divide two numbers 
+def divide(num1, num2): 
+	return division
+	
+def remainder(num1, num2): #Returns remainder when num1 is divided by num2 
+	return remainder
+
+def power(num1, num2): #Returns num1^num2, write your own code and not an inbuilt library
+	return power
+
+# Python 3 Program to print a Geometric_Progression series 
+def Geometric_Progression(a, r, n): # for power function, use your own defined power method above 
+	""" a = 2 # starting number 
+		r = 3 # Common ratio 
+		n = 5 # N th term to be find 
+	Output:
+	2 6 18 54 162
+
+	"""
+	
+	return gp_list 
+
+# Python 3 Program to print an arithmetic progression series 
+def Arithmetic_Progression(a,d,n): 
+	""" 
+	a = 2 # starting number 
+		d = 1 # Common difference 
+		n = 5 # N th term to be find 
+		Output:
+		2 3 4 5 6
+	"""
+	return ap_list 
+	 
+	
+def median_of_two_lists(list_1, list_2):
+	""" Given two lists  list_1 and list_2 of size m and n respectively, return the median of the two sorted arrays.
+	Example 1:
+
+	Input: list_1 = [1,3], list_2 = [2]
+	Output: 2.00000
+	Explanation: merged array = [1,2,3] and median is 2.
+	Example 2:
+
+	Input: list_1 = [1,2], list_2 = [3,4]
+	Output: 2.50000
+	Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+	"""
+	return median_value_of_two_lists
+
+	
+

6aff28f892d41b4965f047ade572e1bbbd3e1c0d
anishabenny <anishabenny2000@gmail.com>
2020-12-02 23:02:48 +0530

    Completed the login and registration functions in the Quiz project

--- Projects/P1 Quiz_via_CSV/database.py
+++ Projects/P1 Quiz_via_CSV/database.py
@@ -1,20 +1,4 @@
 import sqlite3
-import os
-import csv
-import re
-import shutil
-import pandas as pd
-import hashlib
-import time 
-import threading
-import numpy as np
-import sqlite3
-import os
-import tkinter
-from tkinter import *
-os.system('cls')
-
-# sqlite3 login and registration
 
 with sqlite3.connect("project1_quiz_cs384.db") as db:
     cursor=db.cursor()
@@ -35,33 +19,30 @@ total_marks INTEGER NOT NULL
 );
 ''')
 
-
 def registration():
     username= input('Username: ')
     password = input('Password: ')
     name = input('Name: ')
     whatsapp = input('WhatsApp Number: ')
-    h = hashlib.md5(password.encode())
+
     db = sqlite3.connect('project1_quiz_cs384.db')
     c = db.cursor()
-    c.execute("INSERT INTO project1_registration (username,password,name,whatsapp) VALUES (?,?,?,?)",(username,h.hexdigest(),name,whatsapp))
+    c.execute("INSERT INTO project1_registration (username,password,name,whatsapp) VALUES (?,?,?,?)",(username,password,name,whatsapp))
     db.commit()
-    quiz(username,0)
 
 def login():
     user = input('Username: ')
     password = input('Password: ')
-    h = hashlib.md5(password.encode())
+
+        # Connect to database
     db = sqlite3.connect('project1_quiz_cs384.db')
     c = db.cursor()
-    c.execute('SELECT * FROM project1_registration WHERE username = ? AND password = ?', (user, h.hexdigest()))
+    c.execute('SELECT * FROM project1_registration WHERE username = ? AND password = ?', (user, password))
+
     if c.fetchall():
-        quiz(user,1)
+        print('Welcome')
     else:
-        print('Login failed. Kindly register yourself by filling in the following details')
-        registration()
+        print('Login failed')
 
-
-
-print("Kindly login to attempt the Quiz")
+# registration()
 login()
